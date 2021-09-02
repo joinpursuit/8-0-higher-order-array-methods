7dd7977856146cf3eb741cda8958063e4987c663
“Lili <lilyhuang114classb@gmail.com>
2021-09-02 12:59:09 -0400

    Passed 01-forEach

--- src/01-forEach.js
+++ src/01-forEach.js
@@ -12,7 +12,25 @@ const exampleSongData = require("../data/songs");
  * Logs out all of the song titles.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printAllSongTitles(songs) {}
+/*
+const songs = [
+  {
+    title: "Berlin Tsukin",
+    album: "Bi-To Te-Pu",
+    artist: "Taiyo Ky",
+    runtimeInSeconds: 193,
+  },
+  {
+    title: "Up",
+    album: "Bluewerks Vol. 1: Up Down Left Right",
+    artist: "Sebastian Kamae",
+    runtimeInSeconds: 120,
+  }
+]
+*/
+function printAllSongTitles(songs) {
+  songs.forEach(song => console.log(song.title))
+}
 
 /**
  * Logs out the song title and song artist.
@@ -24,13 +42,19 @@ function printAllSongTitles(songs) {}
  *  //> "Up by Sebastian Kamae"
  *  //> ...
  */
-function printSongDetails(songs) {}
+function printSongDetails(songs) {
+  songs.forEach(song => console.log(song.title +" by " + song.artist))
+}
 
 /**
  * Logs out all of the song titles which have a runtime over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printSongTitlesOverThreeMinutes(songs) {}
+function printSongTitlesOverThreeMinutes(songs) {
+  songs.forEach(song => {
+    if(song.runtimeInSeconds/60 > 3) console.log(song.title)
+  })
+}
 
 module.exports = {
   printAllSongTitles,
