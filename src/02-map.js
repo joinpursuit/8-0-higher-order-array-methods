5303a1eed2c5730c743730d733c90860451ab7a2
CarlosAMoran <Carlosamoran18@gmail.com>
2021-09-02 13:10:13 -0400

    First filter test complete

--- src/02-map.js
+++ src/02-map.js
@@ -12,15 +12,6 @@ function getSongDetails(songs) {
   });
 };
 
-/**
- * Returns an array of objects, where each object has a key that is the song title and has a value that is the song artist.
- * @param {Object[]} songs - An array of songs. See the song data for more.
- * @returns {Object[]} An array of objects.
- *
- * EXAMPLE:
- *  getTitleAndArtist(songs);
- *  //> [ { "Berlin Tsukin": "Taiyo Ky" }, { Up: "Sebastian Kamae" }, ... ]
- */
 function getTitleAndArtist(songs) {
   return songs.map((song) => {
     return { [song.title]: song.artist};
--- src/03-filter.js
+++ src/03-filter.js
@@ -1,19 +1,12 @@
-/*
-  Do not change the line below. If you'd like to run code from this file, you may use the `exampleSongData` variable below to gain access to tickets data. This data is pulled from the `data/songs.js` file.
-
-  You may use this data to test your functions. You may assume the shape of the data remains the same but that the values may change.
-
-  Keep in mind that your functions must still have and use a parameter for accepting all songs.
-*/
 const exampleSongData = require("../data/songs");
-// Do not change the line above.
 
-/**
- * Returns an array of all songs by the artist "Saib".
- * @param {Object[]} songs - An array of songs. See the song data for more.
- * @returns {Object[]} An array of objects.
- */
-function getSongsBySaib(songs) {}
+function getSongsBySaib(songs) {
+  return songs.filter((song)=>{
+    if (song.artist === "Saib"){
+      return song;
+    }
+  })
+}
 
 /**
  * Returns an array of all songs where the runtime is over three minutes.
