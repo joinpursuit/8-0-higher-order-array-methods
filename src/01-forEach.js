31858f8be49b4f0b853b4d1e0e940b5b016faa43
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2021-09-02 12:49:17 -0400

    For Each Lab completed with Lakisha

--- src/01-forEach.js
+++ src/01-forEach.js
@@ -1,7 +1,11 @@
 /*
-  Do not change the line below. If you'd like to run code from this file, you may use the `exampleSongData` variable below to gain access to tickets data. This data is pulled from the `data/songs.js` file.
+  Do not change the line below. 
+  If you'd like to run code from this file, 
+  you may use the `exampleSongData` variable below to gain access to tickets data. 
+  This data is pulled from the `data/songs.js` file.
 
-  You may use this data to test your functions. You may assume the shape of the data remains the same but that the values may change.
+  You may use this data to test your functions. 
+  You may assume the shape of the data remains the same but that the values may change.
 
   Keep in mind that your functions must still have and use a parameter for accepting all songs.
 */
@@ -12,11 +16,19 @@ const exampleSongData = require("../data/songs");
  * Logs out all of the song titles.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printAllSongTitles(songs) {}
+function printAllSongTitles(songs) {
+  //forEach 2 ()s
+  //() goes after the forEach function
+  
+  songs.forEach((song) => {
+    console.log(song.title);
+  })
+};
 
 /**
  * Logs out the song title and song artist.
- * @param {Object[]} songs - An array of songs. See the song data for more.
+ * @param {Object[]} songs - An array of songs. 
+ * See the song data for more.
  *
  * EXAMPLE:
  *  printSongDetails(songs);
@@ -24,13 +36,23 @@ function printAllSongTitles(songs) {}
  *  //> "Up by Sebastian Kamae"
  *  //> ...
  */
-function printSongDetails(songs) {}
+function printSongDetails(songs) {
+  songs.forEach((song) => {
+    console.log(song.title + ' ' + 'by' + ' ' + song.artist);
+  })
+}
 
 /**
  * Logs out all of the song titles which have a runtime over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printSongTitlesOverThreeMinutes(songs) {}
+function printSongTitlesOverThreeMinutes(songs) {
+  songs.forEach((song) => {
+    if(song.runtimeInSeconds >= 180){ //60sec * 3 = 180;
+      console.log(song.title);
+    }
+  })
+}
 
 module.exports = {
   printAllSongTitles,
