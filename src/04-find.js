8e6c31b18bbd4b27e50fc9d9df62512ae6f80d48
Rohan-Sterling <rohansterling@pursuit.org>
2021-09-02 13:39:40 -0400

    add solutions for findFirstSongUnderThreeMinutes(songs), remove  variable

--- src/04-find.js
+++ src/04-find.js
@@ -14,8 +14,7 @@ const exampleSongData = require("../data/songs");
  * @returns {Object} A single object.
  */
 function findPinkElephantsByTimestreet(songs) {
-  const found = songs.find(({ title }) => title === "Pink Elephants")
-  return found
+  return songs.find(({ title }) => title === "Pink Elephants")
 }
 
 /**
@@ -23,7 +22,9 @@ function findPinkElephantsByTimestreet(songs) {
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstSongUnderThreeMinutes(songs) {}
+function findFirstSongUnderThreeMinutes(songs) {
+return songs.find(({ runtimeInSeconds }) => runtimeInSeconds < 180)
+}
 
 /**
  * Returns the first song in the list where the song title equals the song album.
