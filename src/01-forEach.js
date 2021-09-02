3b2ac71e1356ca2c1c907317b86d27f9a25e5fd6
Rose Durandisse <rosedurandisse@pursuit.org>
2021-09-02 13:01:20 -0400

    Final For Each File

--- src/01-forEach.js
+++ src/01-forEach.js
@@ -12,7 +12,11 @@ const exampleSongData = require("../data/songs");
  * Logs out all of the song titles.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printAllSongTitles(songs) {}
+function printAllSongTitles(songs) {
+  songs.forEach( ({title}) => {
+      console.log(title)
+  });
+}
 
 /**
  * Logs out the song title and song artist.
@@ -24,13 +28,25 @@ function printAllSongTitles(songs) {}
  *  //> "Up by Sebastian Kamae"
  *  //> ...
  */
-function printSongDetails(songs) {}
+function printSongDetails(songs) {
+  songs.forEach( ({title, artist}) => {
+    console.log( `${title} by ${artist}` )
+  });
+}
 
 /**
  * Logs out all of the song titles which have a runtime over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printSongTitlesOverThreeMinutes(songs) {}
+function printSongTitlesOverThreeMinutes(songs) {
+  songs.forEach( ({title , runtimeInSeconds}) => {
+    if (runtimeInSeconds > 180) {
+    console.log(title)
+    };
+  });
+}
+
+printSongTitlesOverThreeMinutes(exampleSongData) 
 
 module.exports = {
   printAllSongTitles,
