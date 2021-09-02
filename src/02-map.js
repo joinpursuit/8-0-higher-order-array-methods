d1690bbc16db6e91a98d7c908024b8c9fc8eac2b
Younes Tahiri <you@example.com>
2021-09-02 17:00:50 -0400

    Solved forEach, map, filter, and find with Triane Peart

--- src/02-map.js
+++ src/02-map.js
@@ -13,7 +13,9 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {string[]} An array of strings, all of which are song titles.
  */
-function getSongTitles(songs) {}
+function getSongTitles(songs) {
+  return songs.map((song) => song.title);
+}
 
 /**
  * Returns an array of all of the song titles with the artist.
@@ -24,7 +26,9 @@ function getSongTitles(songs) {}
  *  getSongDetails(songs);
  *  //> [ "Berlin Tsukin by Taiyo Ky", "Up by Sebastian Kamae", ... ]
  */
-function getSongDetails(songs) {}
+function getSongDetails(songs) {
+  return songs.map((song) => `${song.title} by ${song.artist}`);
+}
 
 /**
  * Returns an array of objects, where each object has a key that is the song title and has a value that is the song artist.
@@ -35,7 +39,11 @@ function getSongDetails(songs) {}
  *  getTitleAndArtist(songs);
  *  //> [ { "Berlin Tsukin": "Taiyo Ky" }, { Up: "Sebastian Kamae" }, ... ]
  */
-function getTitleAndArtist(songs) {}
+function getTitleAndArtist(songs) {
+  return songs.map((song) => ({
+    [song.title]: song.artist,
+  }));
+}
 
 module.exports = {
   getSongTitles,
