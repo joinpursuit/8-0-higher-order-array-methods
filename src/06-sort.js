2fb818704548dfbd6175d11bec7ce3f497f93645
JoseBorbon <joserborbon@gmail.com>
2021-09-02 11:45:54 -0400

    updated sortJS

--- src/06-sort.js
+++ src/06-sort.js
@@ -13,7 +13,9 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByRuntimeAscending(songs) {}
+function sortByRuntimeAscending(songs) {
+  return songs.sort((firstSong, secondSong) => firstSong.runtimeInSeconds - secondSong.runtimeInSeconds);
+}
 
 /**
  * Reorders the array so that the song objects are organized by their artist name. The artist that comes last in the alphabet should come first.
@@ -23,7 +25,19 @@ function sortByRuntimeAscending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByArtistNameDescending(songs) {}
+function sortByArtistNameDescending(songs) {
+  return songs.sort((firstSong, secondSong) => {
+    const firstLowered = firstSong.artist.toLowerCase();
+    const secondLowered = secondSong.artist.toLowerCase();
+    if (firstLowered > secondLowered) {
+      return -1;
+    }
+    if (firstLowered < secondLowered) {
+      return 1;
+    }
+    return 0;
+  });
+}
 
 /**
  * Reorders the array so that the song objects are organized by their song title. The title that comes first in the alphabet should come first.
@@ -33,7 +47,19 @@ function sortByArtistNameDescending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortBySongTitleAscending(songs) {}
+function sortBySongTitleAscending(songs) {
+  return songs.sort((firstSong, secondSong) => {
+    firstLowered = firstSong.title.toLowerCase();
+    secondLowered = secondSong.title.toLowerCase();
+    if (firstLowered < secondLowered) {
+      return -1;
+    } else if (firstLowered > secondLowered) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+}
 
 module.exports = {
   sortByRuntimeAscending,
