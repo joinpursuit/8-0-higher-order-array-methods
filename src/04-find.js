a14d49dcd339ac8c9b2c60ce47fa8c5d84fde01b
CarlosAMoran <Carlosamoran18@gmail.com>
2021-09-02 13:22:22 -0400

    some-every test 1 complete

--- src/04-find.js
+++ src/04-find.js
@@ -16,11 +16,6 @@ function findFirstSongUnderThreeMinutes(songs) {
   });
 };
 
-/**
- * Returns the first song in the list where the song title equals the song album.
- * @param {Object[]} songs - An array of songs. See the song data for more.
- * @returns {Object} A single object.
- */
 function findFirstTitleTrack(songs) {  
   return songs.find((song) => {
   if (song.title == song.album){
