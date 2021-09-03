a58c8ffac95a6be266e8cee5519724c601213aee
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2021-09-03 14:54:41 -0400

    completed sort functions with Lakisha.

--- src/06-sort.js
+++ src/06-sort.js
@@ -9,21 +9,37 @@ const exampleSongData = require("../data/songs");
 // Do not change the line above.
 
 /**
- * Reorders the array so that the song objects are organized by their runtime. The shortest song should come first.
+ * Reorders the array so that the song objects are organized by their runtime. 
+ * The shortest song should come first.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByRuntimeAscending(songs) {}
+function sortByRuntimeAscending(songs) {
+  return songs.sort((x, y) => {
+    if(x.runtimeInSeconds - y.runtimeInSeconds)
+    return x.runtimeInSeconds - y.runtimeInSeconds
+  })
+}
 
 /**
- * Reorders the array so that the song objects are organized by their artist name. The artist that comes last in the alphabet should come first.
+ * Reorders the array so that the song objects are organized by their artist name. 
+ * The artist that comes last in the alphabet should come first.
  *
- * TIP: "A" and "a" sort differently. To avoid this issue, you may want to use `.toLowerCase()` or `.toUpperCase()`.
+ * TIP: "A" and "a" sort differently. 
+ * To avoid this issue, you may want to use `.toLowerCase()` or `.toUpperCase()`.
  *
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByArtistNameDescending(songs) {}
+function sortByArtistNameDescending(songs) {
+  return songs.sort((song, song2) => {
+    let uno = song.artist.toLowerCase();
+    let dos = song2.artist.toLowerCase();
+    if (uno > dos) return -1; // if this appears first, return a number less than 0
+    if (uno < dos) return 1; //if it appears after the second in the assorted array, it should return a number > 0
+    return 0; //if it's equivalent
+  })
+}
 
 /**
  * Reorders the array so that the song objects are organized by their song title. The title that comes first in the alphabet should come first.
@@ -33,7 +49,16 @@ function sortByArtistNameDescending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortBySongTitleAscending(songs) {}
+function sortBySongTitleAscending(songs) {
+  return songs.sort((song, song2) => {
+    let uno = song.title.toLowerCase();
+    let dos = song2.title.toLowerCase();
+    if (uno < dos) return -1; // if this appears first, return a number less than 0
+    if (uno > dos) return 1; //if it appears after the second in the assorted array, it should return a number > 0
+    return 0; //if it's equivalent
+  })
+}
+
 
 module.exports = {
   sortByRuntimeAscending,
