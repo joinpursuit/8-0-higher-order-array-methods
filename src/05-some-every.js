2bde448d036e957592a2767a4746e2476f3b83fd
imolina212 <86175612+imolina212@users.noreply.github.com>
2021-09-03 15:55:21 -0400

    completed 05 functions

--- src/05-some-every.js
+++ src/05-some-every.js
@@ -13,21 +13,30 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {boolean}
  */
-function allSongsAreOverTwoMinutes(songs) {}
+function allSongsAreOverTwoMinutes(songs) {
+  const songsOverTwo = songs.every((song) => song.runtimeInSeconds > 120);
+  return songsOverTwo;
+}
 
 /**
  * Returns `true` if any song is over four minutes. Otherwise, return `false`.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {boolean}
  */
-function anySongIsOverFourMinutes(songs) {}
+function anySongIsOverFourMinutes(songs) {
+  const songsOverFour = songs.some((song) => song.runtimeInSeconds > 240);
+  return songsOverFour || false;
+}
 
 /**
  * Returns `true` if any song is by the artist "Peanut". Otherwise, return `false`.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {boolean}
  */
-function anySongIsByPeanut(songs) {}
+function anySongIsByPeanut(songs) {
+  let songsByPeanut = songs.some((song) => song.artist === "Peanut");
+  return songsByPeanut || false;
+}
 
 module.exports = {
   allSongsAreOverTwoMinutes,
