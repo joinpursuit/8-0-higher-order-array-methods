da01cbf0caf11d8622b3ad88db75efb3942aee5f
Fatema Kabir <fatema.kabir@pursuit.org>
2021-09-03 16:56:08 -0400

    map

--- src/02-map.js
+++ src/02-map.js
@@ -13,8 +13,10 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {string[]} An array of strings, all of which are song titles.
  */
-function getSongTitles(songs) {}
 
+function getSongTitles(songs) {
+  return songs.map((song) => song.title)
+}
 
 /**
  * Returns an array of all of the song titles with the artist.
@@ -25,8 +27,10 @@ function getSongTitles(songs) {}
  *  getSongDetails(songs);
  *  //> [ "Berlin Tsukin by Taiyo Ky", "Up by Sebastian Kamae", ... ]
  */
-function getSongDetails(songs) {}
 
+function getSongDetails(songs) {
+  return songs.map((song) => song.title + ' by ' + song.artist)
+}
 
 /**
  * Returns an array of objects, where each object has a key that is the song title and has a value that is the song artist.
@@ -37,8 +41,13 @@ function getSongDetails(songs) {}
  *  getTitleAndArtist(songs);
  *  //> [ { "Berlin Tsukin": "Taiyo Ky" }, { Up: "Sebastian Kamae" }, ... ]
  */
- function getTitleAndArtist(songs) {}
-
+ function getTitleAndArtist(songs) {
+  return songs.map((song) => {
+     let objOfSongs = {};
+     return objOfSongs = {[song.title] : song.artist};
+  });
+  return objOfSongs;
+}
 
 module.exports = {
   getSongTitles,
