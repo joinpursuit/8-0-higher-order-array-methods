9c7db5ac141e79d8d47201bfc9aa42cf28897b7e
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2021-09-03 06:44:46 -0400

    completed .every lab with Lakisha.

--- src/05-some-every.js
+++ src/05-some-every.js
@@ -13,21 +13,36 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {boolean}
  */
-function allSongsAreOverTwoMinutes(songs) {}
+function allSongsAreOverTwoMinutes(songs) {
+  return songs.every((song) => {
+    if(song.runtimeInSeconds > 120)
+    return true
+    })
+  }
 
 /**
  * Returns `true` if any song is over four minutes. Otherwise, return `false`.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {boolean}
  */
-function anySongIsOverFourMinutes(songs) {}
+function anySongIsOverFourMinutes(songs) {
+  return songs.some((song) => {
+    if(song.runtimeInSeconds > 240)
+    return true
+    })
+}
 
 /**
  * Returns `true` if any song is by the artist "Peanut". Otherwise, return `false`.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {boolean}
  */
-function anySongIsByPeanut(songs) {}
+function anySongIsByPeanut(songs) {
+  return songs.some((song) => {
+    if(song.artist === "Peanut")
+    return true
+    })
+}
 
 module.exports = {
   allSongsAreOverTwoMinutes,
