53d94b9a3946592efc18299db76a3897c33d9d8d
Sara Perkaj <saraperkaj@pursuit.org>
2021-09-03 13:28:12 -0400

    .every and .some done

--- src/05-some-every.js
+++ src/05-some-every.js
@@ -13,21 +13,24 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {boolean}
  */
-function allSongsAreOverTwoMinutes(songs) {}
+const allSongsAreOverTwoMinutes = (songs) =>
+  songs.every((song) => song.runtimeInSeconds > 120);
 
 /**
  * Returns `true` if any song is over four minutes. Otherwise, return `false`.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {boolean}
  */
-function anySongIsOverFourMinutes(songs) {}
+const anySongIsOverFourMinutes = (songs) =>
+  songs.some((song) => song.runtimeInSeconds > 240);
 
 /**
  * Returns `true` if any song is by the artist "Peanut". Otherwise, return `false`.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {boolean}
  */
-function anySongIsByPeanut(songs) {}
+const anySongIsByPeanut = (songs) =>
+  songs.some((song) => song.artist === "Peanut");
 
 module.exports = {
   allSongsAreOverTwoMinutes,
