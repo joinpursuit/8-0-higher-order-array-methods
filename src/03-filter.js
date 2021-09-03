8565f628b43237c084acb485efdf16642dafe8e0
Christina Loiacono <loiaconochristina@gmail.com>
2021-09-02 22:02:56 -0400

    completed file 03-filter.js

--- src/03-filter.js
+++ src/03-filter.js
@@ -13,21 +13,38 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsBySaib(songs) {}
+function getSongsBySaib(songs) {
+  let answer = songs.filter((song) => {
+  return song.artist === 'Saib'
+  })
+  return answer;
+}
+
+// return songs.filter((song) =>  song.artist === 'Saib');
 
 /**
  * Returns an array of all songs where the runtime is over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsOverThreeMinutes(songs) {}
+function getSongsOverThreeMinutes(songs) {
+  let answer = songs.filter((song) => {
+    return song.runtimeInSeconds > 180
+    })
+    return answer;
+}
 
 /**
  * Returns an array of songs where the song title is the same as the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getTitleTracks(songs) {}
+function getTitleTracks(songs) {
+  let answer = songs.filter((song) => {
+    return song.title === song.album
+    })
+    return answer;
+}
 
 module.exports = {
   getSongsBySaib,
