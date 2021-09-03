41b14c20f37dc0c4b31091ef6505f63e6ed7962d
andres ballares <andr3sb@hotmail.com>
2021-09-03 16:09:47 -0400

    finish with the Lab

--- src/06-sort.js
+++ src/06-sort.js
@@ -13,7 +13,17 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByRuntimeAscending(songs) {}
+function sortByRuntimeAscending(songs) {
+  return songs.sort((song1, song2) => {
+    if(song1.runtimeInSeconds > song2.runtimeInSeconds){
+      return 1;
+    }else if (song2.runtimeInSeconds > song1.runtimeInSeconds){
+      return -1;
+    } else {
+      return 0;
+    }
+  })
+}
 
 /**
  * Reorders the array so that the song objects are organized by their artist name. The artist that comes last in the alphabet should come first.
@@ -23,7 +33,19 @@ function sortByRuntimeAscending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByArtistNameDescending(songs) {}
+function sortByArtistNameDescending(songs) {
+  return songs.sort((song1, song2) => {
+    let art1 = song1.artist.toLowerCase();
+    let art2 = song2.artist.toLowerCase();
+    if(art1 > art2){
+      return -1;
+    } else if (art2 > art1){
+      return 1;
+    }else {
+      return 0;
+    }
+    })
+}
 
 /**
  * Reorders the array so that the song objects are organized by their song title. The title that comes first in the alphabet should come first.
@@ -33,7 +55,19 @@ function sortByArtistNameDescending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortBySongTitleAscending(songs) {}
+function sortBySongTitleAscending(songs) {
+  return songs.sort((song1, song2) => {
+    let title1 = song1.title.toLowerCase();
+    let title2 = song2.title.toLowerCase();
+    if (title1 > title2){
+      return 1;
+    } else if (title2 > title1){
+      return -1;
+    } else {
+      return 0;
+    }
+  })
+}
 
 module.exports = {
   sortByRuntimeAscending,
