b6f936d68386bf45d96afd4e4ee51eeb760d0a6d
Jalamang Janka <jalamangjankagmail.com>
2021-09-02 23:17:53 -0400

    Logging all song title

--- src/01-forEach.js
+++ src/01-forEach.js
@@ -1,18 +1,24 @@
 /*
-  Do not change the line below. If you'd like to run code from this file, you may use the `exampleSongData` variable below to gain access to tickets data. This data is pulled from the `data/songs.js` file.
+  Do not change the line below. If you'd like to run code from this file, you may use 
+  the `exampleSongData` variable below to gain access to tickets data. This data is pulled from 
+  the `data/songs.js` file.
 
-  You may use this data to test your functions. You may assume the shape of the data remains the same but that the values may change.
+  You may use this data to test your functions. You may assume the shape of the data remains the 
+  same but that the values may change.
 
   Keep in mind that your functions must still have and use a parameter for accepting all songs.
 */
 const exampleSongData = require("../data/songs");
 // Do not change the line above.
-
+const log = console.log
 /**
  * Logs out all of the song titles.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printAllSongTitles(songs) {}
+function printAllSongTitles(songs) {
+//for each song object the callback function logs the song's title
+songs.forEach((songTitle) => console.log(songTitle.title))
+}
 
 /**
  * Logs out the song title and song artist.
@@ -24,13 +30,15 @@ function printAllSongTitles(songs) {}
  *  //> "Up by Sebastian Kamae"
  *  //> ...
  */
-function printSongDetails(songs) {}
+function printSongDetails(songs) {
+}
 
 /**
  * Logs out all of the song titles which have a runtime over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
 function printSongTitlesOverThreeMinutes(songs) {}
+
 
 module.exports = {
   printAllSongTitles,
