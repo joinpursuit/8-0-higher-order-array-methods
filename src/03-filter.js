de19bd624f9f8df23887598e7ec56ecd9267ec8a
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2021-09-07 23:10:35 -0400

    filter

--- src/03-filter.js
+++ src/03-filter.js
@@ -8,26 +8,52 @@
 const exampleSongData = require("../data/songs");
 // Do not change the line above.
 
-/**
- * Returns an array of all songs by the artist "Saib".
+/* Returns an array of all songs by the artist "Saib".
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsBySaib(songs) {}
+//filter the songs
+//return saib's songs
+//the final array shall contain all the Saib songs from the song collection and none of  the non Saib songs
+function getSongsBySaib(songs) {
+  let songBySaib = songs.filter((song) => {
+    if (song.artist === "Saib") {
+      return song;
+    }
+  });
+
+  return songBySaib;
+}
 
 /**
  * Returns an array of all songs where the runtime is over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsOverThreeMinutes(songs) {}
+
+function getSongsOverThreeMinutes(songs) {
+  let moreThan = songs.filter((song) => {
+    if (song.runtimeInSeconds > 180) {
+      return song;
+    }
+  });
+
+  return moreThan;
+}
 
 /**
  * Returns an array of songs where the song title is the same as the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getTitleTracks(songs) {}
+function getTitleTracks(songs) {
+  let byTrack = songs.filter((song) => {
+    if (song.title === song.album) {
+      return song;
+    }
+  });
+  return byTrack;
+}
 
 module.exports = {
   getSongsBySaib,
