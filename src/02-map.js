a3f3448737e5f1fa5598b89581ee753373de1578
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-03-08 22:46:58 -0500

    "Two Down"

--- src/02-map.js
+++ src/02-map.js
@@ -13,8 +13,11 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {string[]} An array of strings, all of which are song titles.
  */
+//creates a function to get song titles with the parameter songs which is an array
  function getSongTitles(songs) {
+   //using the map method on the array songs return the iteration 
    return  songs.map((song) => {
+     //return iteration of song title 
      return song.title
    })
  }
@@ -32,7 +35,14 @@ const exampleSongData = require("../data/songs");
  *  getSongDetails(songs);
  *  //> [ "Berlin Tsukin by Taiyo Ky", "Up by Sebastian Kamae", ... ]
  */
-function getSongDetails(songs) {}
+//creates a function to get song details with the parameter songs which is an array 
+function getSongDetails(songs) {
+  //using map method on songs array return iteration through song 
+  return songs.map((song) =>{
+    //return song title by song artist
+    return `${song.title} by ${song.artist}`
+  })
+}
 
 /**
  * Returns an array of objects, where each object has a key that is the song title and has a value that is the song artist.
@@ -43,7 +53,16 @@ function getSongDetails(songs) {}
  *  getTitleAndArtist(songs);
  *  //> [ { "Berlin Tsukin": "Taiyo Ky" }, { Up: "Sebastian Kamae" }, ... ]
  */
-function getTitleAndArtist(songs) {}
+//creat a function to get the tite and artist with the parameter songs which is an array
+function getTitleAndArtist(songs) {
+  //using the map method on the array songs and return the iteration 
+  return songs.map((song) => ({
+    //of song title and song artist as an object 
+    [song.title] : song.artist
+  })
+
+  )
+}
 
 module.exports = {
   getSongTitles,
