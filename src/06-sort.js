64087c8de707b30a8348eaf8d90bc1c2c219a9e5
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-03-10 16:39:09 -0500

    sortByRuntimeAscending()
    Done!

--- src/06-sort.js
+++ src/06-sort.js
@@ -13,7 +13,9 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByRuntimeAscending(songs) {}
+function sortByRuntimeAscending(songs) {
+  return songs.sort((a, b) => a.runtimeInSeconds - b.runtimeInSeconds);
+}
 
 /**
  * Reorders the array so that the song objects are organized by their artist name. The artist that comes last in the alphabet should come first.
