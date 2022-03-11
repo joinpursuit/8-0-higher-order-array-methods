cae4fd289c20603b5e03dd370c571c00a6ea6b52
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-03-11 04:22:07 -0500

    getSongsBySaib()
    Done!

--- src/03-filter.js
+++ src/03-filter.js
@@ -13,7 +13,9 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsBySaib(songs) {}
+function getSongsBySaib(songs) {
+  return songs.filter((song) => song.artist === "Saib"
+  )}
 
 /**
  * Returns an array of all songs where the runtime is over three minutes.
