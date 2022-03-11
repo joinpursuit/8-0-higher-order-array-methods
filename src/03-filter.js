f135b2d3fa534d3518dfa189d711594bb6f783c3
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-03-11 04:23:24 -0500

    getTitleTracks()
    Done!

--- src/03-filter.js
+++ src/03-filter.js
@@ -31,7 +31,9 @@ function getSongsOverThreeMinutes(songs) {
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getTitleTracks(songs) {}
+function getTitleTracks(songs) {
+  return songs.filter((song) => song.title === song.album
+  )}
 
 module.exports = {
   getSongsBySaib,
