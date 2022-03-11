fd439fa51ea41c2424ff259206df271ed611d38c
Victor E Lopez <velbx51594@macbook-air.myfiosgateway.com>
2022-03-11 18:22:08 -0500

    HOAM lab

--- src/06-sort.js
+++ src/06-sort.js
@@ -6,6 +6,7 @@
   Keep in mind that your functions must still have and use a parameter for accepting all songs.
 */
 const exampleSongData = require("../data/songs");
+const { anySongIsOverFourMinutes } = require("./05-some-every");
 // Do not change the line above.
 
 /**
@@ -13,7 +14,10 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByRuntimeAscending(songs) {}
+function sortByRuntimeAscending(songs) {
+     return songs.sort((a,b) => a.runtimeInSeconds - b.runtimeInSeconds)
+  }
+
 
 /**
  * Reorders the array so that the song objects are organized by their artist name. The artist that comes last in the alphabet should come first.
@@ -23,7 +27,9 @@ function sortByRuntimeAscending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByArtistNameDescending(songs) {}
+function sortByArtistNameDescending(songs) {
+  return songs.sort((a,b) => (b.artist.toLowerCase() > a.artist.toLowerCase() ? 1 : -1))
+}
 
 /**
  * Reorders the array so that the song objects are organized by their song title. The title that comes first in the alphabet should come first.
@@ -33,7 +39,9 @@ function sortByArtistNameDescending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortBySongTitleAscending(songs) {}
+function sortBySongTitleAscending(songs) {
+   return songs.sort((a,b) => (a.title.toLowerCase() < b.title.toLowerCase() ? -1 : 1))
+}
 
 module.exports = {
   sortByRuntimeAscending,
