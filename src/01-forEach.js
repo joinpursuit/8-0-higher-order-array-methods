7f9f11d98e0610b03400b40b6e17136e2b90873e
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2022-03-12 16:11:09 -0500

    Higher level

--- src/01-forEach.js
+++ src/01-forEach.js
@@ -5,14 +5,20 @@
 
   Keep in mind that your functions must still have and use a parameter for accepting all songs.
 */
+const songs = require("../data/songs");
 const exampleSongData = require("../data/songs");
+const { sortBySongTitleAscending } = require("./06-sort");
 // Do not change the line above.
 
 /**
  * Logs out all of the song titles.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printAllSongTitles(songs) {}
+function printAllSongTitles (songs) {
+ return songs.forEach((song) => {
+   console.log (song.title)
+ })
+}
 
 /**
  * Logs out the song title and song artist.
@@ -24,13 +30,23 @@ function printAllSongTitles(songs) {}
  *  //> "Up by Sebastian Kamae"
  *  //> ...
  */
-function printSongDetails(songs) {}
+function printSongDetails(songs){
+  return songs.forEach((song) => {
+ console.log (`${song.title} by ${song.artist}`)
+  })
+}
+
 
 /**
  * Logs out all of the song titles which have a runtime over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printSongTitlesOverThreeMinutes(songs) {}
+function printSongTitlesOverThreeMinutes(songs) {
+ return songs.forEach((song) =>  {
+   if (song.runtimeInSeconds > 180)
+   console.log(song.title)
+})
+}
 
 module.exports = {
   printAllSongTitles,
--- src/03-filter.js
+++ src/03-filter.js
@@ -13,21 +13,27 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsBySaib(songs) {}
+function getSongsBySaib(songs) {
+  return songs.filter(song => song.artist === "Saib")
+}
 
 /**
  * Returns an array of all songs where the runtime is over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsOverThreeMinutes(songs) {}
+function getSongsOverThreeMinutes(songs) {
+  return songs.filter(song => song.runtimeInSeconds > 180)
+}
 
 /**
  * Returns an array of songs where the song title is the same as the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getTitleTracks(songs) {}
+function getTitleTracks(songs) {
+  return songs.filter(song => song.title === song.album)
+}
 
 module.exports = {
   getSongsBySaib,
