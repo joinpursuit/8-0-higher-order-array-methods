15cef3ff1b87972f30efe5348a4a7e658947ce2b
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-03-12 10:14:44 -0500

    finish .filter section

--- src/02-map.js
+++ src/02-map.js
@@ -40,8 +40,8 @@ function getSongDetails(songs) {
  *  //> [ { "Berlin Tsukin": "Taiyo Ky" }, { Up: "Sebastian Kamae" }, ... ]
  */
 function getTitleAndArtist(songs) {
-  return songs.map(song => {
-    return { [song.title]: song.artist } 
+  return songs.map(song => { 
+    return { [song.title]: song.artist };  
   });
 }
 
