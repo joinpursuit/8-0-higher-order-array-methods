b619892ae3ff19befb1e3b23d342672382290161
shareeves07 <reeves.shamar1@gmail.com>
2022-03-16 13:03:03 -0400

    completed lab

--- src/01-forEach.js
+++ src/01-forEach.js
@@ -5,14 +5,16 @@
 
   Keep in mind that your functions must still have and use a parameter for accepting all songs.
 */
-const exampleSongData = require("../data/songs");
+const exampleSongData = require('../data/songs');
 // Do not change the line above.
 
 /**
  * Logs out all of the song titles.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printAllSongTitles(songs) {}
+function printAllSongTitles(songs) {
+  songs.forEach((song) => console.log(song.title));
+}
 
 /**
  * Logs out the song title and song artist.
@@ -24,13 +26,21 @@ function printAllSongTitles(songs) {}
  *  //> "Up by Sebastian Kamae"
  *  //> ...
  */
-function printSongDetails(songs) {}
+function printSongDetails(songs) {
+  songs.forEach((song) => console.log(`${song.title} by ${song.artist}`));
+}
 
 /**
  * Logs out all of the song titles which have a runtime over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printSongTitlesOverThreeMinutes(songs) {}
+function printSongTitlesOverThreeMinutes(songs) {
+  songs.forEach((song) => {
+    if (song.runtimeInSeconds > 180) {
+      console.log(song.title);
+    }
+  });
+}
 
 module.exports = {
   printAllSongTitles,
