f9f139d34ef3f0d5571fefb0b8e3ebf13cdea104
KenyaMighty <kenyamighty@pursuit.org>
2022-04-11 00:34:46 -0400

    6refractors

--- src/03-filter.js
+++ src/03-filter.js
@@ -14,29 +14,25 @@ const { printSongTitlesOverThreeMinutes } = require("./01-forEach");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsBySaib(songs) {
-  return songs.filter((songs) {
-    return (song.artist === "Saib")
-  })
-}
+const getSongsBySaib = (songs) => {
+  return songs.filter((song) => song.artist === "Saib")
+  }
 
 /**
  * Returns an array of all songs where the runtime is over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsOverThreeMinutes(songs) {
-  return songs.filter((song) => {
-    return (song.runtimeInSeconds > 100)
-  })
-}
+const getSongsOverThreeMinutes = (songs) => {
+  return songs.filter((song) => song.runtimeInSeconds > 180);
+  }
 
 /**
  * Returns an array of songs where the song title is the same as the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getTitleTracks(songs) {
+const getTitleTracks = (songs) => {
   return songs.filter((song) => {
     return (song.title === song.album)
   })
